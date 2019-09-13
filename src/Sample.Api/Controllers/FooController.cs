857b1efcdda048f15784c84f702de8c800190050
Selvendiran Jayaraman <selvendiranj@gmail.com>
2019-09-13 10:00:24 -0400

    Add project files.

---
+++ src/Sample.Api/Controllers/FooController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Sample.Api.Controllers
+{
+    class output
+    {
+        public string[] values { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class FooController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(output), 200)]
+        [SwaggerOperation(OperationId = "Foos_GetAll")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var result = new output() { values = new string[] { "foo", "bar" } };
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ src/Sample.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Sample.Client;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SampleAPI api = new SampleAPI() { BaseUri = new Uri("http://localhost:5000") };
+            IFoos fooApi = new Foos(api);
+
+            var result = fooApi.GetAllAsync().Result;
+        }
+    }
+}
